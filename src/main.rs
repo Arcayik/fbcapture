61ee0baac4da91c40812854633067181e9b404c4
Blakely Haughton <blakely.haughton@gmail.com>
2023-03-05 13:14:02 -0500

    slightly reworked project system

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ mod buffer; use crate::buffer::*;
 mod image; use crate::image::*;
 
 use std::path::Path;
-
 use std::time::{Duration, Instant};
 
 const CAP: usize = 1024 * 600 * 4;
@@ -11,28 +10,30 @@ const FILEPATH: &str = "/home/blakely/.local/cache/fbvideo/";
 
 fn main () {
     // cmdline args
-    let query = std::env::args().nth(1).expect("No arguments given");
-    let project = std::env::args().nth(2).unwrap_or(String::from("tmp"));
+    let query = std::env::args().nth(1)
+        .expect("No arguments given");
+    let directory = std::env::args().nth(2)
+        .unwrap_or(String::from(FILEPATH));
 
-    // check if project exists
-    match Path::new(format!("{FILEPATH}/{project}").as_str()).exists() {
+    // check if directory exists
+    match Path::new(&directory).exists() {
         true => println!("path exists"),
         false => println!("path doesn't exist")
     }
 
     match query.as_str() {
-        "record" => record(&project),
-        "compile" => compile(&project),
+        "record" => record(&directory),
+        "compile" => compile(&directory),
         _ => panic!("invalid command line argument")
     };
 }
 
-fn record(project: &String) {
+fn record(directory: &String) {
     let mut total: Duration = Duration::ZERO;
 
     for num in 1..30 {
         let start = Instant::now();
-        buffer_to_file(get_buffer("/dev/fb0"), project, num);
+        buffer_to_file(get_buffer("/dev/fb0"), directory, num);
         let duration = start.elapsed();
         total += duration;
 
@@ -43,9 +44,9 @@ fn record(project: &String) {
     }
 }
 
-fn compile(project: &String) {
+fn compile(directory: &String) {
     for num in 1..30 {
-        let mut buffer = get_buffer(format!("{FILEPATH}/default/{num}").as_str());
+        let mut buffer = get_buffer(format!("{FILEPATH}/{directory}/{num}").as_str());
         write_image(convert_pixels(&mut buffer), num);
     }
 }
